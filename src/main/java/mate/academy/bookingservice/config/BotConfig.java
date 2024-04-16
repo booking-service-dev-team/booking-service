99c4cdc85956a1c7c43436be49f15c65e0f3a275
Denis Kaliakin <110353612+DenisKalyakin@users.noreply.github.com>
2024-04-16 16:27:19 +0300

    Update BotConfig.java

--- src/main/java/mate/academy/bookingservice/config/BotConfig.java
+++ src/main/java/mate/academy/bookingservice/config/BotConfig.java
@@ -2,7 +2,7 @@ package mate.academy.bookingservice.config;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Configuration;
 
 // @Configuration
 @Data
